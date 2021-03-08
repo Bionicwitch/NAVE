2fcaf22b425bd45411b32b136312f9e3d47ed9a1
Bionic <ydsrosa@inf.ufpel.edu.br>
2021-03-08 16:57:34 -0300

    Javascript aula 02 Desafio 1

--- JAVASCRIPT AULA 02/script.js
+++ JAVASCRIPT AULA 02/script.js
@@ -1,3 +1,23 @@
-const func = () => {
-    console.log("teste")
+const subtrai = (a, b) => a - b;
+
+const testaZero = (valor) => {
+    switch (valor) {
+        case 0:
+            console.log("é zero");
+            break;
+        default:
+            console.log("não é zero");
+            break;
+    }
+}
+function testaSinal(a,b) {
+    let resultado;
+    resultado=subtrai(a,b);
+    if (resultado == 0) {
+        console.log("é zero");
+    }else if ((a>0 && b<0)||(a<0 && b>0)) {
+        console.log("Subtração entre números de sinais diferentes");
+    }else{
+        console.log("Subtração entre números de mesmo sinal");
+    }
 }
