3fbb40aad15f11da4712299646e2e4a29528d92a
Bionic <ydsrosa@inf.ufpel.edu.br>
2021-03-16 17:18:38 -0300

    Desafio 1 Javascript aula 6

---
+++ JAVASCRIPT AULA 06/Desafio 1.js
@@ -0,0 +1,32 @@
+const user = {
+    nome: 'Usuário',
+    idade: 25,
+    endereco: {
+      rua: 'Rua central',
+      numero: 1
+    },
+    projetos: ['Projeto 1', 'Projeto 2']
+  }
+
+const {nome}=user;
+console.log(nome);
+const {endereco:{rua}}=user;
+console.log(rua);
+const {projetos}=user;
+console.log(projetos);
+const {projetos:[,x]}=user;
+console.log(x);
+
+const students = [
+    {id: 1, name: 'John Doe', age: 23, scholarship: false},
+    {id: 2, name: 'Margoe Rose', age: 19, scholarship: true},
+    {id: 3, name: 'Kayle', age: 22, scholarship: true}
+]
+
+
+const [primeiro]=students;
+console.log(primeiro);
+const [,,terceiro]=students;
+console.log(terceiro);
+const [,{name:nomezinho}]=students;
+console.log(nomezinho);
